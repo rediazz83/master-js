7ac857caec16201524b0c3030bf6918609873cb5
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-02 20:44:20 -0300

    Uso de parametros REST y SPREAD

--- funciones/js/01-funciones.js
+++ funciones/js/01-funciones.js
@@ -4,10 +4,24 @@ console.log("01-funciones.js");
 haceAlgo(1, 2);
 haceAlgo(1, 2, 3);
 
+listadoFrutas("pera", "manzana", "melon", "durazno", "frutilla", "platano");
+
+//Pasando parametro SPREAD: ...frutas
+var frutas = ["pera", "manzana", "melon", "durazno", "frutilla", "platano"];
+listadoFrutas(...frutas, "pomelo", "naranja");
+
 //Funcion parametro opcional
 //parametroOpcional = 0 -> valor por defecto
 function haceAlgo(primerNumero, segundoNumero, parametroOpcional = 0) {
   console.log("primerNumero: " + primerNumero);
   console.log("segundoNumero: " + segundoNumero);
   console.log("parametroOpcional:" + parametroOpcional);
+}
+
+//Parametros REST
+function listadoFrutas(...arregloFrutas) {
+  console.log(arregloFrutas);
+  for(var i = 0; i < arregloFrutas.length; i++) {
+    console.log("Fruta[" + i + "]: " + arregloFrutas[i]);
+  }
 }
