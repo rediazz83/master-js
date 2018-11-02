19c28979126a4b2343c5c6bffb41fe53f35fdaf7
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-02 05:43:29 -0300

    Se activa el modo estricto

--- hola-mundo/js/01-hola-mundo.js
+++ hola-mundo/js/01-hola-mundo.js
@@ -1,3 +1,4 @@
+"use strict"
 console.log("01-hola-mundo.js");
 
 document.write("<h5>Hola Mundo - Fichero Externo</h5>");
--- hola-mundo/js/02-variables.js
+++ hola-mundo/js/02-variables.js
@@ -1,4 +1,7 @@
+"use strict"
+console.log("02-variables.js");
+
 var pais = "España";
 var continente = "Europa"
 var antiguedad = 2019;
-var paisMasContinente = pais + " - " + continente; 
+var paisMasContinente = pais + " - " + continente;
